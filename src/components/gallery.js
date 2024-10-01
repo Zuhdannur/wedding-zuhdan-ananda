5d0c1f3431a7be0558ffff667778a4794d4b59a9
zuhdan Nur Ihsan Iskandar <pczuhdan33@gmail.com>
2024-10-01 23:12:27 +0700

    update

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -12,14 +12,14 @@ import Img06 from '../assets/images/gallery/06.jpg'
 export default function Example({ open, setOpen }) {
 
     return (
-        <Dialog open={open} onClose={setOpen} className="relative z-[9999999] h-3/4 w-full max-w-md">
+        <Dialog open={open} onClose={setOpen} className="relative z-[9999999] w-full max-w-md">
             <DialogBackdrop
                 transition
                 className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
             />
 
-            <div className="fixed top-0 z-[99999] w-screen overflow-y-scroll">
-                <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0 ">
+            <div className="fixed h-screen top-0 z-[99999] w-screen overflow-y-scroll">
+                <div className="flex items-end justify-center p-4 text-center sm:items-center sm:p-0 ">
                     <DialogPanel
                         transition
                         className="relative transform overflow-x-hidden rounded-lg bg-white text-left shadow-xl border-[2px] border-primary transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
